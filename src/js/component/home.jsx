98f26f96cae922453711a0de476d508f8e878bc4
jmmonzonn <jmmonzonn@gmail.com>
2022-02-16 00:06:58 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bigDiv">
+			<div className="littleDiv">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="littleDiv">{props.thousands % 10}</div>
+			<div className="littleDiv">{props.hundreds % 10}</div>
+			<div className="littleDiv">{props.tens % 10}</div>
+			<div className="littleDiv">{props.unit % 10}</div>
 		</div>
 	);
 };
+
+Home.propTypes = {
+	unit: PropTypes.number,
+	tens: PropTypes.number,
+	hundreds: PropTypes.number,
+	thousands: PropTypes.number,
+};
+
+let counter = 0;
+
+setInterval(function () {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter);
+
+	counter++;
+	ReactDOM.render(
+		<Home unit={one} tens={two} hundreds={three} thousands={four} />,
+		document.querySelector("#app")
+	);
+}, 1000);
 
 export default Home;
